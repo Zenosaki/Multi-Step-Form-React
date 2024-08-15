6127dc35705914b2a8beaf31aa7bc4907d13e061
Zenosaki <arselin.chaabanie@gmail.com>
2024-08-15 15:54:39 +0200

    little touches

--- src/components/StepsContents/Step4.jsx
+++ src/components/StepsContents/Step4.jsx
@@ -4,7 +4,7 @@ import Title from '../StepContentTitle'
 export default function Step4(){
   return(
     <div className="Step-Content-Container">
-      <Title title='Finishing up' description='Double-check everything looks OK before confirming'/>
+      <Title title='Finishing up' description='Double-check everything looks OK before confirming.'/>
       <div className="content"></div>
     </div>
   )
