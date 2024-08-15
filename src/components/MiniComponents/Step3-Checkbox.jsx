679e04be59204a66592afb5724bd108e40c4a602
Zenosaki <arselin.chaabanie@gmail.com>
2024-08-15 15:07:20 +0200

    finished designing step 3

--- src/components/MiniComponents/Step3-Checkbox.jsx
+++ src/components/MiniComponents/Step3-Checkbox.jsx
@@ -1,24 +1,31 @@
-import PropTypes from 'prop-types'
+import Tick from '../../assets/images/icon-checkmark.svg';
+import PropTypes from 'prop-types';
 
+const SetActive = (event) => {
+  const element = event.currentTarget;
+  element.classList.toggle('Tactive');
+};
 
-export default function Checkbox(props){
-  return(
-    <div className="checkBox">
+export default function Checkbox(props) {
+  return (
+    <div onClick={SetActive} className="checkBox">
       <div className="LeftSide">
-        <div className="checkIcon"></div>
+        <div className="checkIcon">
+          <img src={Tick} alt="Tick Icon" />
+        </div>
         <div className="service">
           <h4>{props.service}</h4>
-          <h4 className='ServiceP'>{props.specs}</h4>
+          <h4 className="ServiceP">{props.specs}</h4>
         </div>
       </div>
-      <h4 className='Price'>+${props.price}/{props.symbol}</h4>
+      <h4 className="Price">+${props.price}/{props.symbol}</h4>
     </div>
-  )
+  );
 }
 
-Checkbox.propTypes ={
-  service: PropTypes.string,
-  specs: PropTypes.string,
-  price: PropTypes.number,
-  symbol: PropTypes.string,
-}
+Checkbox.propTypes = {
+  service: PropTypes.string.isRequired,
+  specs: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  symbol: PropTypes.string.isRequired,
+};
