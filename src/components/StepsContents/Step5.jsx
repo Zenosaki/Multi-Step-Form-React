dc84ea9addeb9e8f931a6e01349027b37e9e60cb
Zenosaki <arselin.chaabanie@gmail.com>
2024-08-16 17:37:23 +0200

    finished styling the step5 and the structure

--- src/components/StepsContents/Step5.jsx
+++ src/components/StepsContents/Step5.jsx
@@ -1,10 +1,12 @@
-
+import thxIcone from '../../assets/images/icon-thank-you.svg'
 
 
 export default function Step5(){
   return(
     <div className="Step5-container">
-      <h1>THank you</h1>
+      <img src={thxIcone} alt="icone" />
+      <h1>Thank you!</h1>
+      <p>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</p>
     </div>
   )
 }
