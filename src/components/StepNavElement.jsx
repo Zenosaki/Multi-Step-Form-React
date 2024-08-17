ea54e04a8a1f43e7f3cbcedb2e4d7e509d87bb70
Zenosaki <arselin.chaabanie@gmail.com>
2024-08-17 15:49:54 +0200

    i finish the responsivness of the project

--- src/components/StepNavElement.jsx
+++ src/components/StepNavElement.jsx
@@ -6,7 +6,7 @@ export default function StepElement(props) {
       <div>
         <h4 className={`Step ${props.Step == props.currentS || props.currentS>4 && props.Step===4  ? 'Step-Active' : ''}`}>{props.Step}</h4>
       </div>
-      <div>
+      <div className='StepDiscription'>
         <h5 className="StepIndicator">Step {props.Step}</h5>
         <h4 className="StepTitle">{props.StepTitle}</h4>
       </div>
